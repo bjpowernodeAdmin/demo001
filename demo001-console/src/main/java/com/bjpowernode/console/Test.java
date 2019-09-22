9ea5e43ee10f86662cda2a6b9bc8898531f2acd0
何山 <1045972365@qq.com>
2019-09-22 17:39:53 +0800

    [config][heshan][add user1 function]

--- demo001-console/src/main/java/com/bjpowernode/console/Test.java
+++ demo001-console/src/main/java/com/bjpowernode/console/Test.java
@@ -1,4 +1,11 @@
 package com.bjpowernode.console;
 
 public class Test {
+
+    public static void main(String[] args) {
+
+        User2.function2();
+
+    }
+
 }
---
+++ demo001-console/src/main/java/com/bjpowernode/console/User2.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.console;
+
+public class User2 {
+
+    public static void function2(){
+
+        System.out.println("234");
+
+    }
+
+}
